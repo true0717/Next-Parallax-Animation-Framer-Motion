a64c9de99589f022e8869f3cd3dfedded9c24f67
yuyasano0096 <mackbus0096@gmail.com>
2024-12-23 13:36:03 +0530

    Next JS Parallax Landing Page

---
+++ src/app/layout.js
@@ -0,0 +1,15 @@
+
+import './globals.css'
+
+export const metadata = {
+  title: 'Next JS Zoom Parallax',
+  description: 'Next JS Zoom Parallax',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
